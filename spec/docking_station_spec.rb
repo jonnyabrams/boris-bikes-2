6d3efd019ed05190cd45cf4f23c27dd251678457
Jonny Abrams <jonnyabrams@hotmail.com>
2022-02-24 15:41:40 +0000

    Added 1st unit test & defined release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,8 @@
 require 'docking_station'
-describe DockingStation do
 
+describe DockingStation do
+  it 'responds to release_bike' do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to(:release_bike)
+  end
 end    
